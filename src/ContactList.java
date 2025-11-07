dad80f1ab6ff99f1b94b6c60de379a864af831b3
khaled <aa19ss2019@gmail.com>
2025-11-07 13:30:22 +0300

    هذا التعديل الاخير

--- src/ContactList.java
+++ src/ContactList.java
@@ -2,13 +2,46 @@ import java.util.Scanner;
 
 public class ContactList {
     public static void main(String[] args) {
-        System.out.println("Добавь следующее" );
-        System.out.println("1 - Имя " );
-        System.out.println("2 - Номер" );
         Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        int numberPhone = in.nextInt();
-        System.out.println("Имя: "+ name);
-        System.out.print("Номер: "+ numberPhone);
+        boolean running = true;
+        int indexArray=10;
+        int i=0;
+        String array[]= new String[indexArray];
+        int arrayNumber[] = new  int[indexArray];
+        while (running) {
+            System.out.println("\n=== Простой список контактов ===");
+            System.out.println("1. Добавить контакт");
+            System.out.println("2. Показать все контакты");
+            System.out.println("3. Выйти");
+            System.out.print("Выберите действие: ");
+            int selectedNumber= in.nextInt();
+
+            switch(selectedNumber){
+                case 1 :
+                    System.out.print("Напиши имя: ");
+                    in.nextLine();
+                    String name = in.nextLine();
+                    System.out.print("Номер : ");
+                    int numberPhone= in.nextInt();
+                    array[i]=name;
+                    arrayNumber[i]= numberPhone;
+                    i=i+1;
+                    break;
+                case 2 :
+                    for(int j =0 ; j < i; j++ ){
+                        System.out.println("id :" + (1 + j) );
+                        System.out.println("Имя :"+ array[j]);
+                        System.out.println("Номер : " + arrayNumber[j] );
+                        System.out.println("::::::::::::::::::::::::::::::::::"  );
+                    }
+                    break;
+                case 3 :
+                    running=false;
+                    break;
+                default:
+                    System.out.println("not this number");
+            }
+        }
+
     }
 }
